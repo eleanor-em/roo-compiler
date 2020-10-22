53dee3872a1b89ddbe27e7b16ddb269e649931be
Eleanor McMurtry <elmcmurtry1@gmail.com>
2020-10-22 19:21:32 +1100

    minor line length refactors

--- Oz.hs
+++ Oz.hs
@@ -16,19 +16,24 @@ ozPopStackFrame :: Int -> [Text]
 ozPopStackFrame n = ["pop_stack_frame " <> tshow n]
 
 ozLoad :: Register -> StackSlot -> [Text]
-ozLoad register location = ["load " <> tshow register <> ", " <> tshow location]
+ozLoad register location
+    = ["load " <> tshow register <> ", " <> tshow location]
 
 ozLoadAddress :: Register -> StackSlot -> [Text]
-ozLoadAddress register location = ["load_address " <> tshow register <> ", " <> tshow location]
+ozLoadAddress register location
+    = ["load_address " <> tshow register <> ", " <> tshow location]
 
 ozLoadIndirect :: Register -> Register -> [Text]
-ozLoadIndirect value pointer = ["load_indirect " <> tshow value <> ", " <> tshow pointer]
+ozLoadIndirect value pointer
+    = ["load_indirect " <> tshow value <> ", " <> tshow pointer]
 
 ozStore :: StackSlot -> Register -> [Text]
-ozStore location register = ["store " <> tshow location <> ", " <> tshow register]
+ozStore location register
+    = ["store " <> tshow location <> ", " <> tshow register]
 
 ozStoreIndirect :: Register -> Register -> [Text]
-ozStoreIndirect pointer value = ["store_indirect " <> tshow pointer <> ", " <> tshow value]
+ozStoreIndirect pointer value
+    = ["store_indirect " <> tshow pointer <> ", " <> tshow value]
 
 ozMove :: Register -> Register -> [Text]
 ozMove dest src = ["move " <> tshow dest <> ", " <> tshow src]
@@ -37,7 +42,8 @@ ozBinOp :: Text -> Register -> Register -> [Text]
 ozBinOp op dest src = pure $ op <> " " <> tshow dest <> ", " <> tshow src
 
 ozTernOp :: Text -> Register -> Register -> Register -> [Text]
-ozTernOp op dest lhs rhs = pure $ op <> " " <> tshow dest <> ", " <> tshow lhs <> ", " <> tshow rhs
+ozTernOp op dest lhs rhs
+    = [op <> " " <> tshow dest <> ", " <> tshow lhs <> ", " <> tshow rhs]
 
 ozNot, ozNeg :: Register -> Register -> [Text]
 ozNot = ozBinOp "not"
@@ -61,23 +67,27 @@ ozDivide = ozTernOp "div_int"
 ozSubOffset = ozTernOp "sub_offset"
 
 ozIntConst :: Register -> Int -> [Text]
-ozIntConst register val = ["int_const " <> tshow register <> ", " <> tshow val]
+ozIntConst register val
+    = ["int_const " <> tshow register <> ", " <> tshow val]
 
 ozBoolConst :: Register -> Bool -> [Text]
-ozBoolConst register val = ["int_const " <> tshow register <> ", " <> convertBool val]
+ozBoolConst register val
+    = ["int_const " <> tshow register <> ", " <> convertBool val]
 
 ozWriteInt :: Register -> [Text]
 ozWriteInt (Register 0) = ["call_builtin print_int"]
-ozWriteInt register = ozMove (Register 0) register <> ["call_builtin print_int" ]
+ozWriteInt register
+    = ozMove (Register 0) register <> ["call_builtin print_int" ]
 
 ozWriteBool :: Register -> [Text]
-ozWriteBool (Register 0) = ["call_builtin print_bool"]
-ozWriteBool register = ozMove (Register 0) register <> ["call_builtin print_bool"]
+ozWriteBool (Register 0)
+    = ["call_builtin print_bool"]
+ozWriteBool register
+    = ozMove (Register 0) register <> ["call_builtin print_bool"]
 
 ozWriteString :: Text -> [Text]
-ozWriteString val =
-    [ "string_const r0, \"" <> val <> "\""
-    , "call_builtin print_string" ]
+ozWriteString val
+    = [ "string_const r0, \"" <> val <> "\"", "call_builtin print_string" ]
 
 ozReadInt :: [Text]
 ozReadInt = [ "call_builtin read_int" ]
@@ -86,10 +96,12 @@ ozReadBool :: [Text]
 ozReadBool = [ "call_builtin read_bool" ]
 
 ozBranchOnTrue :: Register -> Text -> [Text]
-ozBranchOnTrue register label = ["branch_on_true " <> tshow register <> ", " <> label]  
+ozBranchOnTrue register label
+    = ["branch_on_true " <> tshow register <> ", " <> label]  
 
 ozBranchOnFalse :: Register -> Text -> [Text]
-ozBranchOnFalse register label = ["branch_on_false " <> tshow register <> ", " <> label] 
+ozBranchOnFalse register label
+    = ["branch_on_false " <> tshow register <> ", " <> label] 
 
 ozBranch :: Text -> [Text]
 ozBranch label = ["branch_uncond " <> label]
